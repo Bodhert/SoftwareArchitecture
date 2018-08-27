7eaa05e02ba8208730f43bf56466c121a3d06986
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-08-26 21:11:44 -0500

    client logic done

--- RentingCars/src/app/ConsoleApp.java
+++ RentingCars/src/app/ConsoleApp.java
@@ -1,24 +1,109 @@
 package app;
+import java.util.ArrayList;
+import java.util.List;
+import transactionHelper.*;
+import java.util.Scanner;
+
 import carHandler.Car;
 import carHandler.FamilyCar;
 import carHandler.Garage;
 
 public class ConsoleApp 
 {
-
+	private String command = "";
+	private Scanner scanner = new Scanner(System.in);
+	private List<Cliente> clientes;
+	private List<RentarTransaccion> transacciones;
+	private Cliente currentClient;
+	private Garage garage;
 	
-	public static void main(String[] args) 
+	void storeCars()
 	{
-		Car test1 = new FamilyCar("213", "232", "dos", 1600, 4, 1, "regular");
-		Car test2 = new FamilyCar("2133", "2332", "dofs", 1600, 4, 1, "regularpan");
-		Garage garage = Garage.getGarage();
+		Car test1 = new FamilyCar("123", "Renault", "mecanico", 1600, 4, 10, "completo");
+		Car test2 = new FamilyCar("234", "Fiat", "automatico", 1200, 4, 10, "contra choques");
+		Car test3 = new FamilyCar("456", "Lada", "mecanico", 2000, 4, 10, "completo");
 		
 		garage.addCar(test1);
-		garage.addCar(test2);		
-//		garage.printGarage();
-		garage.removeCar(test2);
-		garage.removeCar(test2);
-//		garage.printGarage();
+		garage.addCar(test2);
+		garage.addCar(test3);
 	}
+	
+	void getCommand()
+	{
+		command = scanner.next();
+	}
+	
+	void printOptions()
+	{
+		System.out.println("Presiona");
+		System.out.println("c para ingresar o buscar cliente");
+		System.out.println("t para Realizar Transaccion carro");
+		System.out.println("f para finalizar");
+	}
+	
+	
+	public Cliente getClient(String id)
+	{
+		 for(Cliente temp : clientes)
+		 {
+	        if(temp.getId() != null && temp.getId().equals(id))
+	        {
+	        	return temp;
+	        }
+		 }
+		 return null;
+	}
+	
+	public void ClientLogic(String id)
+	{
+		System.out.println("ingreseId:");
+		String nombre;
+		boolean is_vip;
+		currentClient = getClient(id);
+		if(currentClient == null)
+		{
+			System.out.println("cliente no encontrado");
+			System.out.println("ingrese nombre");
+			nombre = scanner.next();
+			currentClient = new Cliente(id, nombre, true); // for now everyone is vip
+			clientes.add(currentClient);
+		}	
+	}
+	
+	public void businnesLogic()
+	{
+		getCommand();
+		while(!command.equalsIgnoreCase("F"))
+		{
+//			System.out.println("holiwis");
+			if(!isValidCommand()) System.out.println("comando no reconocido");
+			else
+			{
+				if(command.equalsIgnoreCase("C"))
+				{
+					System.out.println("ingrese el id del cliente");
+					String id = scanner.next();
+					ClientLogic(id);
+				}
+			}
+			getCommand();
+		}
+	}
+	
+	boolean isValidCommand()
+	{
+		return command.equalsIgnoreCase("C")|| command.equalsIgnoreCase("T") ||
+				command.equalsIgnoreCase("F");
+	}
+	
+	public ConsoleApp()
+	{
+		clientes = new ArrayList<>();
+		transacciones = new ArrayList<>();
+ 		garage = Garage.getGarage();
+		storeCars();
+		printOptions();
+	}
+	
 
 }
--- RentingCars/src/app/Main.java
+++ RentingCars/src/app/Main.java
@@ -5,7 +5,7 @@ import it.sauronsoftware.junique.JUnique;
 
 public class Main {
 	
-	static ConsoleApp app;
+	static ConsoleApp app = new ConsoleApp();
 	public static void main(String[] args) {
 		final String name = "aplication_Unica";
 		boolean alreadyRuning;
@@ -21,6 +21,8 @@ public class Main {
 		if (!alreadyRuning) 
 		{
 			System.out.println("jejejej no corre xd");
+			app.businnesLogic();
+			System.out.println("Programa Terminado");
 		}
 		else
 		{
